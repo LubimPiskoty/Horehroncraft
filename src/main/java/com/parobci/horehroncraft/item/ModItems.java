a0c976e720955a6e0424fb03d5f5852081fe01f9
LubimPiskoty <54437068+LubimPiskoty@users.noreply.github.com>
2022-01-24 22:22:59 +0100

    KTH pridana 🖤

---
+++ src/main/java/com/parobci/horehroncraft/item/ModItems.java
@@ -0,0 +1,25 @@
+package com.parobci.horehroncraft.item;
+
+import com.parobci.horehroncraft.HorehronCraft;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =  
+        DeferredRegister.create(ForgeRegistries.ITEMS, HorehronCraft.MOD_ID);
+
+
+    public static final RegistryObject<Item> TEMNA_HMOTA = ITEMS.register("koncentrovana_temna_hmota", 
+        () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
